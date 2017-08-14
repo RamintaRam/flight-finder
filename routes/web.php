b9a28c60b5ee6b57ae4e857bedcbcc2542fe8f0d
RamintaRam <raminta5@yahoo.com>
2017-08-14 19:24:04 +0300

    main page is flight-search

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@
 |
 */
 //
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
-Route::get('/search', ['as' => 'app.search.index', 'uses' => 'SearchFlightController@index']);
+Route::get('/', ['as' => 'app.search.index', 'uses' => 'SearchFlightController@index']);
 
 //
 //return;
